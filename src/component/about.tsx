18613fe64fbde26a127b7c8f9f3f69d9c5d807fe
furqanshah77 <shahfurqan668@gmail.com>
2024-10-17 01:53:11 +0500

    `Feat`: -edit some thing

--- src/component/about.tsx
+++ src/component/about.tsx
@@ -9,9 +9,9 @@ const About = () => {
       <div className="h-[140px]" />
       <div className="grid sm:grid-cols-2 md:grid-cols-4 items-center text-white gap-y-10">
         <Info discription="Furqan Ali" text="Full Name" />
-        <Info text="Email Address" discription="Expamle@gmail.com" />
+        <Info text="Email Address" discription="shahfurqan668@gmail.com" />
         <Info text="Socail" discription="#" />
-        <Info text="Phone" discription="035412" />
+        <Info text="Phone" discription="03102270213" />
       </div>
     </div>
   );
